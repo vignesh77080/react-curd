7930301376263b753e1a9f516bd5d3b62cb24d48
vignesh77080 <vignesh.yes97@gmail.com>
2020-04-21 14:38:02 +0530

    Updates

---
+++ precache-manifest.d44d56503cc51bb13907f1473ac1e6a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca7a9d2f5970199666712a3b047c46a",
+    "url": "/react-curd/index.html"
+  },
+  {
+    "revision": "71f46de4cc0fbdeffc7f",
+    "url": "/react-curd/static/css/main.6bea8036.chunk.css"
+  },
+  {
+    "revision": "9059e40ed8fffbbc9bdc",
+    "url": "/react-curd/static/js/2.6eb184d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-curd/static/js/2.6eb184d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f46de4cc0fbdeffc7f",
+    "url": "/react-curd/static/js/main.9b9f3e90.chunk.js"
+  },
+  {
+    "revision": "7f9e39dcb82b40492d08",
+    "url": "/react-curd/static/js/runtime-main.e1709c5d.js"
+  }
+]);
